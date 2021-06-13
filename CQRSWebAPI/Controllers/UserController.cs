ef1681b89be94558a9993c444e56dac03adf50c8
borhan azhdari <borhanaz1379@gmail.com>
2021-06-13 16:31:56 +0430

    Project init

---
+++ CQRSWebAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using CQRSWebAPI.Query;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRSWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public UserController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        [HttpGet("/{Id}")]
+        public async Task<IActionResult> GetUserbyName(int Id, CancellationToken cancellationToken )
+        {
+            var res = await mediator.Send(new GetUserById.Query(Id), cancellationToken);
+            
+            return res==null ? NotFound() : Ok(res);
+        }
+    }
+}
---
+++ CQRSWebAPI/Model/SeedData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSWebAPI.Model
+{
+    public class SeedData
+    {
+        public List<User> Users { get; } = new List<User>
+        {
+            new User{ Id=1 , Name = "John" , IsActive = true  },
+            new User{ Id=2 , Name = "Tom" , IsActive = true  },
+            new User{ Id=3 , Name = "Polly" , IsActive = true  },
+            new User{ Id=4 , Name = "Ada" , IsActive = true  },
+            new User{ Id=5 , Name = "Amit" , IsActive = true  },
+            new User{ Id=6 , Name = "Sarah" , IsActive = false  },
+            new User{ Id=7 , Name = "Borhan" , IsActive = true  },
+            new User{ Id=8 , Name = "Fin" , IsActive = true  },
+            new User{ Id=9 , Name = "Arthur" , IsActive = true  },
+        };
+    }
+}
