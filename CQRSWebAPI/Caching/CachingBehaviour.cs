bc672e0c3b0b8ca2ad138b1bbeffecbc93bd4f77
borhan azhdari <borhanaz1379@gmail.com>
2021-06-15 00:49:40 +0430

    Memory Cache Behaviour Update

---
+++ CQRSWebAPI/Caching/CachingBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CQRSWebAPI.Caching
+{
+    public class CachingBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : ICacheable
+    {
+        private readonly IMemoryCache memoryCache;
+        private readonly ILogger<CachingBehaviour<TRequest, TResponse>> logger;
+
+        public CachingBehaviour(IMemoryCache memoryCache, ILogger<CachingBehaviour<TRequest, TResponse>> logger)
+        {
+            this.memoryCache = memoryCache;
+            this.logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var reqName = request.GetType();
+            logger.LogInformation("{Request} is configured for caching.", reqName);
+
+            TResponse response;
+            if (memoryCache.TryGetValue(request.CacheKey, out response))
+            {
+                logger.LogInformation("Returning cached value for {Request}.", reqName);
+                return response;
+            }
+
+            logger.LogInformation("{Request} CacheKey: {Key} is not inside the cache , executing request.", reqName, request.CacheKey);
+            response = await next();
+            memoryCache.Set(request.CacheKey, response);
+            return response;
+        }
+    }
+}
