6446b921c18892096673d950bba6749b003c36b1
borhan azhdari <borhanaz1379@gmail.com>
2021-06-14 23:32:19 +0430

    EndPoint Changes

--- CQRSWebAPI/Controllers/UserController.cs
+++ CQRSWebAPI/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using CQRSWebAPI.Query;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,10 +18,12 @@ namespace CQRSWebAPI.Controllers
     public class UserController : ControllerBase
     {
         private readonly IMediator mediator;
+        //private readonly ILogger<UserController> logger;
 
-        public UserController(IMediator mediator)
+        public UserController(IMediator mediator/*, ILogger<UserController> logger*/)
         {
             this.mediator = mediator;
+            //this.logger = logger;
         }
         [HttpGet("{Id}")]
         public async Task<IActionResult> GetUserbyName(int Id, CancellationToken cancellationToken)
