94122304f3ca484fc5e349ba709f0beca201d8e3
borhan azhdari <borhanaz1379@gmail.com>
2021-06-15 17:48:35 +0430

    validation Added

--- CQRSWebAPI/Command/AddUser.cs
+++ CQRSWebAPI/Command/AddUser.cs
@@ -1,4 +1,6 @@
-﻿using CQRSWebAPI.Model;
+﻿using CQRSWebAPI.DTOs;
+using CQRSWebAPI.Model;
+using CQRSWebAPI.Validation;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -11,10 +13,29 @@ namespace CQRSWebAPI.Command
     public class AddUser
     {
         // Command
-        public record Command(string Name, bool IsActive) : IRequest<User>;
-        // Handler
+        public record Command(string Name, bool IsActive) : IRequest<Response<User>>;
 
-        public class Handler : IRequestHandler<Command, User>
+
+        // Validator 
+        public class Validator : IValidationHandler<Command>
+        {
+            private readonly SeedData data;
+
+            public Validator(SeedData data) => this.data = data;
+
+
+            public async Task<ValidationResult> Validation(Command request)
+            {
+                
+                if (data.Users.Any(x => x.Name.Equals(request.Name, StringComparison.OrdinalIgnoreCase)))
+                    return ValidationResult.Fail("User Already Exist");
+                
+                return ValidationResult.Success;
+            }
+        }
+
+        // Handler
+        public class Handler : IRequestHandler<Command, Response<User>>
         {
             private readonly SeedData data;
 
@@ -22,12 +43,17 @@ namespace CQRSWebAPI.Command
             {
                 this.data = data;
             }
-            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Response<User>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var user = new User { Id = 10, Name = request.Name, IsActive = request.IsActive };
                 data.Users.Add(user);
-                return user; 
-            }   
+                return new Response<User> { data = user };
+            }
+        }
+        public class Response<T> : ResponseDto
+        {
+            public T data { get; set; }
+
         }
     }
 }
