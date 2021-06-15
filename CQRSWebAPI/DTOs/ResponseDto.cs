580c23ead32c92f469f6654d9049f04664679f05
borhan azhdari <borhanaz1379@gmail.com>
2021-06-15 16:10:41 +0430

    Response Dto

---
+++ CQRSWebAPI/DTOs/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CQRSWebAPI.DTOs
+{
+    public class ResponseDto
+    {
+        public HttpStatusCode HttpStatusCode { get; init; } = HttpStatusCode.OK;
+        public string ErrorMessaging { get; init; }
+    }
+}
