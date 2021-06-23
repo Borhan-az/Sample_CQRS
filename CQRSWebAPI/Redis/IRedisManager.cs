57dab803ac1e09e09370d6d9f990db19cf980235
borhan azhdari <borhanaz1379@gmail.com>
2021-06-23 13:14:19 +0430

    Redis manager with Service.Redis POCO updated

---
+++ CQRSWebAPI/Redis/IRedisManager.cs
@@ -0,0 +1,17 @@
+﻿using CQRSWebAPI.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSWebAPI.Redis
+{
+    public interface IRedisManager
+    {
+        bool Create<T>(string cacheKey, T value);
+        T Get<T>(string key);
+        string GetString(string key);
+        bool CreateEventSourcing(EventData data);
+        void DeleteApiCache(Type inputModelType);
+    }
+}
