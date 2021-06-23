d65870d09f629186f65b7f48902817dd33aad195
borhan azhdari <borhanaz1379@gmail.com>
2021-06-23 13:13:46 +0430

    Event Model and Extension Updated

---
+++ CQRSWebAPI/DTOs/EventData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CQRSWebAPI.DTOs
+{
+    public class EventData
+    {
+        public string APIName { get; set; }
+        public DateTime Date { get; set; }
+        public string Response { get; set; }
+        public string Request { get; set; }
+    }
+}
---
+++ CQRSWebAPI/Extension/stringSerialize.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CQRSWebAPI.Extension
+{
+    public static class stringSerialize
+    {
+        public static string Serilize(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+    }
+}
